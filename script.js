457926cf6c4d0cd019067e6170f56dc0cd641d65
christiancba2011 <christiancba2011@hotmail.com>
2024-06-18 15:12:09 -0300

    Prueba Carrusel

--- script.js
+++ script.js
@@ -1,90 +1,75 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const navLinks = document.getElementById('nav-links');
-
-    hamburgerMenu.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     const groupButtons = document.querySelectorAll('.group-btn');
-    const knockoutButtons = document.querySelectorAll('.knockout-btn');
+    const phaseButtons = document.querySelectorAll('.phase-btn');
     const carouselTrack = document.querySelector('.carousel-track');
     const prevBtn = document.querySelector('.prev');
     const nextBtn = document.querySelector('.next');
     let currentIndex = 0;
-    let numVisibleCards = 1; // Default number of visible cards
+    let numVisibleCards = 1; // Número de tarjetas visibles por defecto
+    let startX = 0;
+    let endX = 0;
+    let isTouchMove = false;
 
     // Fetch JSON data
     fetch('https://project-data-games.onrender.com/groups')
         .then(response => response.json())
         .then(data => {
-            populateCarousel(data.groups, "Grupo A"); // Load initial group
+            populateCarousel(data.groups, "Grupo A"); // Cargar grupo inicial
         });
 
-    function populateCarousel(data, selectedGroupOrPhase) {
-        carouselTrack.innerHTML = ''; // Clear existing content
-        
-        // Determine if we are handling groups or knockout phase
-        let matches = data[selectedGroupOrPhase]?.matches || data[selectedGroupOrPhase] || [];
-
-        matches.forEach(match => {
-            const card = createMatchCard(selectedGroupOrPhase, match);
+    function populateCarousel(groups, selectedGroup) {
+        carouselTrack.innerHTML = ''; // Limpiar contenido existente
+        let group = groups[selectedGroup];
+        group.matches.forEach(match => {
+            const card = createMatchCard(selectedGroup, match);
             carouselTrack.appendChild(card);
         });
-        updateControls(); // Update controls after populating carousel
-        updateCarousel(); // Initial positioning
+        updateControls(); // Actualizar controles después de poblar el carrusel
+        updateCarousel(); // Posicionamiento inicial
     }
 
     function createMatchCard(groupName, match) {
         const card = document.createElement('div');
         card.classList.add('card');
-
-        // Check if the groupName is knockout stage
-        const isKnockoutStage = ["quarterfinals", "semifinals", "thirdPlace", "final"].includes(groupName);
-
+        const imgHtml = groupName.includes('Cuartos') || groupName.includes('Semifinal') || groupName.includes('Final') ? '' : `<img src="${match.imageUrl}" alt="Partido ${match.team1} vs ${match.team2}">`;
         card.innerHTML = `
             <h3>${match.team1} vs ${match.team2}</h3>
-            ${!isKnockoutStage ? `<img src="${match.imageUrl}" alt="Partido ${match.team1} vs ${match.team2}">` : ''}
+            ${imgHtml}
             <div class="card-content">
                 <p>${match.dayOfWeek}, ${match.date} - ${match.time}</p>
-                <p>${match.location}</p>
-                <p><strong>Resultado:</strong> ${match.score1 !== null ? match.score1 : '-'} - ${match.score2 !== null ? match.score2 : '-'}</p>
+                <p>Ubicación: ${match.location}</p>
             </div>
         `;
         return card;
     }
 
-    function handleButtonClick(buttons, selectedType) {
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                // Remove 'active' class from all buttons
-                groupButtons.forEach(btn => btn.classList.remove('active'));
-                knockoutButtons.forEach(btn => btn.classList.remove('active'));
-                
-                // Add 'active' class to clicked button
-                button.classList.add('active');
-                
-                const selectedGroupOrPhase = button.getAttribute('data-group');
-                currentIndex = 0; // Reset index when group changes
-                fetch('db.json')
-                    .then(response => response.json())
-                    .then(data => {
-                        if (selectedType === 'group') {
-                            populateCarousel(data.groups, selectedGroupOrPhase);
-                        } else {
-                            populateCarousel(data, selectedGroupOrPhase);
-                        }
-                    });
-            });
+    groupButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            groupButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            const selectedGroup = button.getAttribute('data-group');
+            currentIndex = 0; // Restablecer índice cuando cambia el grupo
+            populateCarouselFromGroup(selectedGroup);
         });
-    }
+    });
 
-    handleButtonClick(groupButtons, 'group');
-    handleButtonClick(knockoutButtons, 'knockout');
+    phaseButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            phaseButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            const selectedGroup = button.getAttribute('data-group');
+            currentIndex = 0; // Restablecer índice cuando cambia la fase
+            populateCarouselFromGroup(selectedGroup);
+        });
+    });
+
+    function populateCarouselFromGroup(selectedGroup) {
+        fetch('db.json')
+            .then(response => response.json())
+            .then(data => {
+                populateCarousel(data.groups, selectedGroup);
+            });
+    }
 
     prevBtn.addEventListener('click', () => {
         if (currentIndex > 0) {
@@ -103,10 +88,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function updateCarousel() {
         const cardWidth = document.querySelector('.card').offsetWidth;
-        const gap = 10; // Assuming a gap between cards
+        const gap = 10; // Asumiendo un espacio entre tarjetas
         const containerWidth = document.querySelector('.carousel-container').offsetWidth;
 
-        // Calculate number of visible cards based on the container width
+        // Calcular el número de tarjetas visibles basado en el ancho del contenedor
         numVisibleCards = Math.floor(containerWidth / (cardWidth + gap));
 
         const moveDistance = (cardWidth + gap) * currentIndex;
@@ -115,15 +100,44 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function updateControls() {
-        // Disable previous button if at the start
+        // Deshabilitar botón anterior si está al inicio
         prevBtn.disabled = currentIndex === 0;
 
-        // Disable next button if at the end
+        // Deshabilitar botón siguiente si está al final
         const numCards = carouselTrack.children.length;
         nextBtn.disabled = currentIndex >= numCards - numVisibleCards;
     }
 
     window.addEventListener('resize', updateCarousel);
+
+    // Agregar eventos de toque para deslizamiento
+    carouselTrack.addEventListener('touchstart', handleTouchStart);
+    carouselTrack.addEventListener('touchmove', handleTouchMove);
+    carouselTrack.addEventListener('touchend', handleTouchEnd);
+
+    function handleTouchStart(event) {
+        startX = event.touches[0].clientX;
+        isTouchMove = false;
+    }
+
+    function handleTouchMove(event) {
+        endX = event.touches[0].clientX;
+        isTouchMove = true;
+    }
+
+    function handleTouchEnd() {
+        if (isTouchMove) {
+            if (startX > endX) {
+                // Deslizar hacia la izquierda
+                nextBtn.click();
+            } else if (startX < endX) {
+                // Deslizar hacia la derecha
+                prevBtn.click();
+            }
+        }
+        // Reiniciar variables de toque
+        startX = 0;
+        endX = 0;
+        isTouchMove = false;
+    }
 });
-
-
